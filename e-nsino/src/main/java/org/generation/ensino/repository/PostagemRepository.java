2dcb8e3cac6a787c6f4ecdc22a066e2985fbfc02
Jaqueline <jaqueline.feitosa.sousa@gmail.com>
2021-12-10 11:36:38 -0300

    Postagem Repository

---
+++ e-nsino/src/main/java/org/generation/ensino/repository/PostagemRepository.java
@@ -0,0 +1,17 @@
+package org.generation.ensino.repository;
+
+import java.util.List;
+
+import org.generation.ensino.Model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	public List<Postagem> findAllByTextoContainingIgnoreCase(String texto);
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	public List<Postagem> findAllByDataContainingIgnoreCase(String data);
+	public List<Postagem> findAllByFotoContainingIgnoreCase(String foto);
+
+    
+}
